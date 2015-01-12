7b663dcb808966d5ec6223e27d9ab8f0690dfd56
David Molineus <david.molineus@netzmacht.de>
2015-01-12 17:01:54 +0100

    Add translations.

---
+++ module/languages/en/modules.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+$GLOBALS['TL_LANG']['MOD']['leaflet-libs'][0] = 'Leaflet libraries';
+$GLOBALS['TL_LANG']['MOD']['leaflet-libs'][1] = 'Leaflet libraries for the Leaflet integration into Contao CMS';
