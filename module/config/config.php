7d8c0da0d263de895f8a22c31f96114e98453571
David Molineus <david.molineus@netzmacht.de>
2015-10-15 22:16:30 +0200

    Update to leaflet v.0.7.5

--- module/config/config.php
+++ module/config/config.php
@@ -17,7 +17,7 @@
 $GLOBALS['LEAFLET_LIBRARIES']['leaflet'] = array
 (
     'name'       => 'Leaflet',
-    'version'    => '0.7.3',
+    'version'    => '0.7.5',
     'license'    => '<a href="https://github.com/Leaflet/Leaflet/blob/master/LICENSE" target="_blank">BSD-2-Clause</a>',
     'homepage'   => 'http://leafletjs.com',
     'css'        => 'assets/leaflet/libs/leaflet/leaflet.min.css',
