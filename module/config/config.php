9f2d5d94dc2c14a1d0225526216e0f166cf338ab
David Molineus <david.molineus@netzmacht.de>
2016-11-13 21:53:08 +0100

    Implementing extra markers.

--- module/config/config.php
+++ module/config/config.php
@@ -82,6 +82,16 @@ $GLOBALS['LEAFLET_LIBRARIES']['leaflet-control-geocoder'] = array
     'javascript' => 'assets/leaflet/libs/control-geocoder/Control.Geocoder.min.js'
 );
 
+$GLOBALS['LEAFLET_LIBRARIES']['leaflet-extra-markers'] = array
+(
+    'name'       => 'Leaflet Extra Markers',
+    'version'    => '1.0.6',
+    'license'    => '<a href="https://github.com/coryasilva/Leaflet.ExtraMarkers/blob/master/LICENSE" target="_blank">MIT</a>',
+    'homepage'   => 'https://github.com/coryasilva/Leaflet.ExtraMarkers',
+    'css'        => 'assets/leaflet/libs/control-extra-marker/leaflet.extra-markers.min.css',
+    'javascript' => 'assets/leaflet/libs/control-extra-marker/leaflet.extra-markers.min.js'
+);
+
 $GLOBALS['LEAFLET_LIBRARIES']['osmtogeojson'] = array
 (
     'name'       => 'osmtogeojson',
